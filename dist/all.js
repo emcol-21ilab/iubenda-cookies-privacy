ba6c5cf3d117ba91cb73c1957f0c48ffb425357c
emcol-21ilab <emanuele.colabello@21ilab.com>
2022-02-19 17:43:33 +0100

    last updates

--- dist/all.js
+++ dist/all.js
@@ -12,83 +12,33 @@ function _arrayWithoutHoles(arr) { if (Array.isArray(arr)) return _arrayLikeToAr
 
 function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
 
-var seatsContainer = document.querySelector('.seats_container'); //seats non occupied
+// catching dom elements
+var closeButton = document.querySelector(".close-btn");
 
-var seats = seatsContainer.querySelectorAll('.seat:not(.occupied)');
-var allSeats = seatsContainer.querySelectorAll('.seat'); //results
+var toggler = _toConsumableArray(document.querySelectorAll('.toggle'));
 
-var count = document.getElementById('count');
-var total = document.getElementById('total'); //select
+var resetButton = document.getElementById('reset'); //event listeners
+//hide the alert from UI
 
-var movieSelect = document.getElementById('movie_picker'); //select price value as number
+closeButton.addEventListener("click", function (e) {
+  e.preventDefault;
+  document.querySelector("".concat(closeButton.dataset.target)).classList.add('hide');
+}); //toggler handler
 
-var ticketPrice = +movieSelect.value; //Get data from localstorage and populate UI
+toggler.forEach(function (el) {
+  el.addEventListener("click", function (e) {
+    console.log(e.target.dataset.target);
+    document.querySelector("".concat(e.target.dataset.target)).classList.toggle('hide');
+  });
+});
+resetButton.addEventListener("click", function (e) {
+  e.preventDefault; //reset form
 
-var populateUI = function populateUI() {
-  var selectedSeats = JSON.parse(localStorage.getItem('selectedSeats')); //check if value exists in the storage
+  document.querySelector('form').reset(); //hide 2 container
 
-  if (selectedSeats !== null && selectedSeats.length > 0) {
-    allSeats.forEach(function (seat, index) {
-      //if an index seat match with the localstorage then update UI
-      if (selectedSeats.indexOf(index) > -1) {
-        seat.classList.add('selected');
-      }
-    });
-  }
-
-  var selectedMovieIndex = localStorage.getItem('selectedMovieIndex'); //check if value exists in the storage
-
-  if (selectedMovieIndex !== null) {
-    movieSelect.selectedIndex = selectedMovieIndex;
-  }
-}; //update total and count function
-
-
-var updateSelectedCount = function updateSelectedCount() {
-  //convert nodelist of selected seats into arr
-  var selectedSeats = _toConsumableArray(seatsContainer.querySelectorAll(".seat.selected")); //Map through array
-  //return a new array of indexes, each index indentifies a seat
-
-
-  var seatsIndex = selectedSeats.map(function (seat) {
-    return _toConsumableArray(allSeats).indexOf(seat);
-  }); // Save selected seats indexes to storage
-
-  setSeatsData(seatsIndex); //updating UI with new total and count
-
-  var selectedSeatsCount = selectedSeats.length;
-  count.innerText = selectedSeatsCount;
-  total.innerText = selectedSeatsCount * ticketPrice;
-}; // Save selected movie index and price to storage
-
-
-var setMovieData = function setMovieData(movieIndex, moviePrice) {
-  localStorage.setItem('selectedMovieIndex', movieIndex);
-  localStorage.setItem('selectedMoviePrice', moviePrice);
-}; // Save selected seats indexes to storage
-
-
-var setSeatsData = function setSeatsData(seatsIndex) {
-  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
-}; //***** HANDLERS
-//movie click event handler
-
-
-movieSelect.addEventListener('change', function (e) {
-  ticketPrice = +e.target.value; //HTMLSelectElement.selectedIndex reflects the index of the first or last selected <option> element
-
-  setMovieData(e.target.selectedIndex, e.target.value);
-  updateSelectedCount();
-}); //seat click event handler
-
-seatsContainer.addEventListener('click', function (e) {
-  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-    e.target.classList.toggle('selected');
-    updateSelectedCount();
-  }
-}); //Get data from localstorage and populate UI
-
-populateUI(); //initial count and total set
-
-updateSelectedCount();
+  _toConsumableArray(document.querySelectorAll(".form-container")).forEach(function (el) {
+    el.classList.remove('hide');
+    el.classList.add('hide');
+  });
+});
 //# sourceMappingURL=all.js.map
--- src/js/script.js
+++ src/js/script.js
@@ -1,90 +1,32 @@
-const seatsContainer = document.querySelector('.seats_container');
-//seats non occupied
-const seats = seatsContainer.querySelectorAll('.seat:not(.occupied)');
-const allSeats = seatsContainer.querySelectorAll('.seat');
-//results
-const count = document.getElementById('count');
-const total = document.getElementById('total');
-//select
-const movieSelect = document.getElementById('movie_picker');
-//select price value as number
-let ticketPrice = +movieSelect.value;
+// catching dom elements
+const closeButton = document.querySelector(".close-btn");
+const toggler = [...document.querySelectorAll('.toggle')];
+const resetButton = document.getElementById('reset');
 
-//Get data from localstorage and populate UI
-const populateUI = () => {
-    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+//event listeners
 
-    //check if value exists in the storage
-    if (selectedSeats !==  null && selectedSeats.length > 0) {
-        
-        allSeats.forEach((seat, index) => {
-            //if an index seat match with the localstorage then update UI
-            if(selectedSeats.indexOf(index) > -1) {
-                seat.classList.add('selected');
-            }
-        });
-    }
-
-    const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
-    //check if value exists in the storage
-    if (selectedMovieIndex !==  null) {
-        movieSelect.selectedIndex = selectedMovieIndex;
-    }
-}
-
-//update total and count function
-const updateSelectedCount = () => {
-
-    //convert nodelist of selected seats into arr
-    const selectedSeats = [...seatsContainer.querySelectorAll(".seat.selected")];
-    
-    //Map through array
-    //return a new array of indexes, each index indentifies a seat
-    const seatsIndex = selectedSeats.map(seat => [...allSeats].indexOf(seat));
-   
-    // Save selected seats indexes to storage
-    setSeatsData(seatsIndex);
-
-    //updating UI with new total and count
-    const selectedSeatsCount = selectedSeats.length;
-    count.innerText = selectedSeatsCount;
-    total.innerText = selectedSeatsCount * ticketPrice;
-}
-
-// Save selected movie index and price to storage
-const setMovieData = (movieIndex, moviePrice) => {
-    localStorage.setItem('selectedMovieIndex', movieIndex);
-    localStorage.setItem('selectedMoviePrice', moviePrice);
-};
-// Save selected seats indexes to storage
-const setSeatsData = (seatsIndex) => {
-    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
-};
-
-
-
-
-//***** HANDLERS
-
-//movie click event handler
-movieSelect.addEventListener('change', e => {
-    ticketPrice = +e.target.value;
-    //HTMLSelectElement.selectedIndex reflects the index of the first or last selected <option> element
-    setMovieData(e.target.selectedIndex, e.target.value);
-
-    updateSelectedCount();
-});
-//seat click event handler
-seatsContainer.addEventListener('click', e => {
-    if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-        e.target.classList.toggle('selected');
-
-        updateSelectedCount();
-    }
+//hide the alert from UI
+closeButton.addEventListener("click", e => {
+    e.preventDefault;
+    document.querySelector(`${closeButton.dataset.target}`).classList.add('hide');
 });
 
+//toggler handler
 
-//Get data from localstorage and populate UI
-populateUI();
-//initial count and total set
-updateSelectedCount();
+toggler.forEach(el => {
+    el.addEventListener("click", e => {
+        console.log(e.target.dataset.target);
+        document.querySelector(`${e.target.dataset.target}`).classList.toggle('hide');
+    });
+})
+
+resetButton.addEventListener("click", e => {
+    e.preventDefault;
+    //reset form
+    document.querySelector('form').reset();
+    //hide 2 container
+    [...document.querySelectorAll(`.form-container`)].forEach(el => {
+        el.classList.remove('hide');
+        el.classList.add('hide');
+    })
+});
